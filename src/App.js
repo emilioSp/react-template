4c1bf7b1e04f5d1b466eb20435b0975317f664fe
emilioSp <emilio.spatola@gmail.com>
2020-11-26 13:09:23 +0100

    Classes are dead!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TimeContainer } from './TimeContainer';
+import { Time } from './Time';
 
-const App = () => {
-  return <TimeContainer/>
-};
+const App = () => <Time/>;
 
 ReactDOM.render(<App/>, document.getElementById('app'));
--- src/Time.js
+++ src/Time.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export const Time = ({currentTime}) => {
-  return <div>Current time: {currentTime}</div>
+export const Time = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => setTime(new Date()), 1000);
+  }, [time]);
+
+  return <h1>Current time: {time.toLocaleString()}</h1>
 };
