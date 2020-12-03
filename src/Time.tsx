1945c8ae040cee89a1bf36a30664262e9ca62c3e
emilioSp <emilio.spatola@gmail.com>
2020-12-03 12:15:08 +0100

    typescript

--- src/Time.tsx
+++ src/Time.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 export const Time = () => {
   const [time, setTime] = useState(new Date());
