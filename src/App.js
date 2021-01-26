05bd85fbae2121133ae1954e321e58724d1e46d1
emilioSp <emilio.spatola@gmail.com>
2021-01-26 21:57:34 +0100

    Update template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Time } from './Time';
 
-const App = () => <Time />;
-
-ReactDOM.render(<App />, document.getElementById('app'));
+export const App = () => <Time />;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,82 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: './src/App.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+const srcPath = path.resolve(__dirname, 'src');
+const buildPath = path.resolve(__dirname, 'dist');
+const isProduction = process.env.NODE_ENV === 'production';
+const optimization = isProduction
+  ? {
+      minimizer: [
+        // `...` is the webpack@5 syntax to extend existing minimizers (i.e. `terser-webpack-plugin`)
+        `...`,
+        new CssMinimizerPlugin(),
+      ],
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/](monaco-editor)[\\/]/, // monaco-editor has a huge bundle size
+            name: 'vendor',
+            chunks: 'all',
+          },
         },
       },
+    }
+  : {};
+
+module.exports = {
+  entry: `${srcPath}/index.js`,
+  devtool: isProduction ? false : 'source-map',
+  devServer: {
+    contentBase: buildPath,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0', // To expose contents via docker
+  },
+  mode: isProduction ? 'production' : 'development',
+  module: {
+    rules: [
+      { test: /\.(m)?js$/, use: ['babel-loader'], exclude: /node_modules/ },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(s)?css$/,
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ico|png|jpg|jpeg|svg|ttf)$/i,
+        type: 'asset',
+        generator: { filename: '[name].[contenthash][ext]' },
       },
     ],
   },
+  optimization,
+  output: {
+    filename: '[name].[contenthash].js',
+    path: buildPath,
+    publicPath: '',
+  },
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
+    // Removes/cleans build folders and unused assets when rebuilding
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${srcPath}/index.html`,
+      filename: 'index.html',
     }),
+    // Extracts CSS into separate files
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
   ],
+  // https://github.com/webpack/webpack-dev-server/issues/2758#issuecomment-706840237
+  // https://webpack.js.org/configuration/target/
+  target: isProduction ? 'browserslist' : 'web',
 };
